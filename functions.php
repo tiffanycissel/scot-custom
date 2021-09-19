3be9453806349255c0c128c35ef6bc1d2f9bce59
Tiffany C <tiffany.cissel@gmail.com>
2021-09-19 18:15:37 -0400

    Set-up basic theme structure

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+// Enqueue stylesheets
+add_action( 'wp_enqueue_scripts', 'enqueue_scot_custom_styles' );
+function enqueue_scot_custom_styles(){
+    $parenthandle = 'twentytwenty-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+    $theme = wp_get_theme();
+    wp_enqueue_style( $parenthandle, get_template_directory_uri() . '/style.css', 
+        array(),  // if the parent theme code has a dependency, copy it to here
+        $theme->parent()->get('Version')
+    );
+    wp_enqueue_style( 'child-style', get_stylesheet_uri(),
+        array( $parenthandle ),
+        $theme->get('Version') // this only works if you have Version in the style header
+    );
+}
