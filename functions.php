81bd0c80cae8d4e121586cd8c944b4dbc829106f
Tiffany C <tiffany.cissel@gmail.com>
2021-11-07 17:47:00 -0500

    Create custom header

--- functions.php
+++ functions.php
@@ -72,3 +72,65 @@ function scot_newsletter_custom_sort( $query ){
         return $query;
     }
 }
+
+/**
+ * Displays the site logo, text and/or image.
+ *
+ * A custom, child theme version of the function
+ * twentytwenty_site_logo (themes\twentytwenty\inc\template-tags.php:25) 
+ * 
+ */
+function scotcustom_site_logo( $output = 'standard', $args = array(), $echo = true ) {
+	$logo       = get_custom_logo();
+	$site_title = get_bloginfo( 'name' );
+	$contents   = '';
+	$classname  = '';
+
+	$defaults = array(
+		'logo'        => '%1$s<span class="screen-reader-text">%2$s</span>',
+		'logo_class'  => 'site-logo',
+		'title'       => '<a href="%1$s">%2$s</a>',
+		'title_class' => 'site-title',
+		'home_wrap'   => '<h1 class="%1$s">%2$s</h1>',
+		'single_wrap' => '<div class="%1$s faux-heading">%2$s</div>',
+		'condition'   => ( is_front_page() || is_home() ) && ! is_page(),
+        'both'        => '%1$s<span class="screen-reader-text">%2$s</span><a href="%3$s">%4$s</a>',
+        'both_class'  => 'site-logo site-title'
+	);
+
+	$args = wp_parse_args( $args, $defaults );
+
+	$args = apply_filters( 'twentytwenty_site_logo_args', $args, $defaults );
+
+    if( $output === 'standard' ){
+        if ( has_custom_logo() ) {
+            $contents  = sprintf( $args['logo'], $logo, esc_html( $site_title ) );
+            $classname = $args['logo_class'];
+        } else {
+            $contents  = sprintf( $args['title'], esc_url( get_home_url( null, '/' ) ), esc_html( $site_title ) );
+            $classname = $args['title_class'];
+        }
+    } elseif( $output === 'logo' ){
+        $contents  = sprintf( $args['logo'], $logo, esc_html( $site_title ) );
+        $classname = $args['logo_class'];
+    } elseif( $output === 'title' ){
+        $contents  = sprintf( $args['title'], esc_url( get_home_url( null, '/' ) ), esc_html( $site_title ) );
+        $classname = $args['title_class'];
+    } elseif ($output === 'both' ){
+        $contents  = sprintf( $args['both'], $logo, esc_html( $site_title ), esc_url( get_home_url( null, '/' ) ), esc_html( $site_title ) );
+        $classname = $args['both_class'];
+    }	
+
+	$wrap = $args['condition'] ? 'home_wrap' : 'single_wrap';
+
+	$html = sprintf( $args[ $wrap ], $classname, $contents );
+
+	$html = apply_filters( 'twentytwenty_site_logo', $html, $args, $classname, $contents );
+
+	if ( ! $echo ) {
+		return $html;
+	}
+
+	echo $html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+
+}
